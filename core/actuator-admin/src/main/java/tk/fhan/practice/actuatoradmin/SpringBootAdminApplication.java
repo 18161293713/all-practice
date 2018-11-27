b0c3da24779bc9764b5bda955ce8495ec60c1b46
hans.f.han <hans.f.han@yzw.cn>
2018-11-27 18:43:14 +0800

    填加spring boot admin监控服务

---
+++ core/actuator-admin/src/main/java/tk/fhan/practice/actuatoradmin/SpringBootAdminApplication.java
@@ -0,0 +1,34 @@
+package tk.fhan.practice.actuatoradmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author: hans.f.han
+ * @description:
+ * @date: Created in 17:11 2018/11/27
+ * @modified By:
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
